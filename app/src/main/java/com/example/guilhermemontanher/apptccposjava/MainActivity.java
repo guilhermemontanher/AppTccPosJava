80bd91aa7607a09c2a5d0b309961be0a20813132
gui_m <gs.montanher@gmail.com>
2019-02-24 22:46:50 -0300

    Versão 2

--- app/src/main/java/com/example/guilhermemontanher/apptccposjava/MainActivity.java
+++ app/src/main/java/com/example/guilhermemontanher/apptccposjava/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.guilhermemontanher.apptccposjava;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.databinding.DataBindingUtil;
+import android.os.SystemClock;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,13 +11,17 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -26,9 +33,19 @@ import com.example.guilhermemontanher.apptccposjava.listener.RecyclerViewButtonC
 import com.example.guilhermemontanher.apptccposjava.model.Param;
 import com.example.guilhermemontanher.apptccposjava.view.adapter.RecyclerAdapterParams;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * @author Guilherme Montanher
+ * @version 1.0
+ * @since 20/01/2019
+ */
 public class MainActivity extends AppCompatActivity implements RecyclerViewButtonClick {
 
     private ActivityMainBinding binding;
@@ -36,6 +53,9 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewButto
     private List<Param> paramList = new ArrayList<>();
 
     private RecyclerAdapterParams recyclerAdapterParams;
+
+    private long startTime = 0;
+    private long endTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,24 +65,77 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewButto
         binding.imageButtonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                hideKeyboard(MainActivity.this);
                 RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
                 String url = binding.editTextUrl.getText().toString();
                 int method = binding.spinnerMethod.getSelectedItemPosition();
 
+                //Constructor with method, url, response listener and error response listener
+                startTime = SystemClock.elapsedRealtime();
                 StringRequest stringRequest = new StringRequest(method, url,
                         new Response.Listener<String>() {
+                            @SuppressLint("SetJavaScriptEnabled")
                             @Override
                             public void onResponse(String response) {
-                                binding.textViewOutPut.setText(response);
+                                endTime = SystemClock.elapsedRealtime();
+                                binding.webViewPage.getSettings().setJavaScriptEnabled(true);
+                                binding.webViewPage.loadDataWithBaseURL("", response, "text/html", "UTF-8", "");
+                                StringBuilder output = new StringBuilder();
+                                output.append("Successful requisition!\r\n");
+                                output.append("Volley time - " + String.valueOf(endTime - startTime));
                                 Snackbar.make(binding.rootView, "Successful requisition!", Snackbar.LENGTH_LONG).show();
                             }
                         }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Snackbar.make(binding.rootView, "That didn't work!", Snackbar.LENGTH_LONG).show();;
-                    }
-                });
+                        final Snackbar snackBar = Snackbar.make(binding.rootView, "That didn't work!", Snackbar.LENGTH_INDEFINITE);
+                        snackBar.setAction("Try again", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                binding.imageButtonSend.callOnClick();
+                                snackBar.dismiss();
+                            }
+                        });
+                        snackBar.show();
 
+                    }
+                }) { //Optional methods, used for authentitacion, passing parameters, etc
+                    @Override
+                    public Map<String, String> getHeaders() {
+                        Map<String, String> headers = new HashMap<>();
+
+                        if (binding.spinnerAuth.getSelectedItemPosition() == 1) {
+                            String credentials = binding.editTextUsername.getText() + ":" + binding.editTextPassword.getText();
+                            String auth = "Basic "
+                                    + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+                            headers.put("Content-Type", "application/json");
+                            headers.put("Authorization", auth);
+                        }
+
+                        return headers;
+                    }
+
+                    @Override
+                    public String getBodyContentType() {
+                        return super.getBodyContentType();
+                    }
+
+                    @Override
+                    public byte[] getBody() {
+                        HashMap<String, String> params = new HashMap<String, String>();
+
+                        for (Param param : paramList) {
+                            params.put(param.getName(), param.getValue());
+                        }
+                        return new JSONObject(params).toString().getBytes();
+                    }
+                };
+
+                try {
+                    Log.d("Headers", stringRequest.getHeaders().toString());
+                } catch (AuthFailureError authFailureError) {
+                    authFailureError.printStackTrace();
+                }
                 queue.add(stringRequest);
             }
         });
@@ -70,7 +143,7 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewButto
         binding.buttonAddParam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!binding.editTextParamName.getText().toString().isEmpty()){
+                if (!binding.editTextParamName.getText().toString().isEmpty()) {
                     paramList.add(
                             new Param(binding.editTextParamName.getText().toString(),
                                     binding.editTextParamValue.getText().toString())
@@ -92,11 +165,41 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewButto
         recyclerAdapterParams = new RecyclerAdapterParams(this, paramList);
         binding.recyclerViewParam.setAdapter(recyclerAdapterParams);
         recyclerAdapterParams.setOnClickRecyclerViewButtonClick(this);
+
+        binding.spinnerAuth.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        binding.viewBasicAuth.setVisibility(View.GONE);
+                        break;
+                    case 1:
+                        binding.viewBasicAuth.setVisibility(View.VISIBLE);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override
     public void onClick(int position) {
         paramList.remove(position);
         recyclerAdapterParams.notifyDataSetChanged();
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
---
+++ app/src/main/java/com/example/guilhermemontanher/apptccposjava/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.guilhermemontanher.apptccposjava;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }.execute();
+    }
+}
